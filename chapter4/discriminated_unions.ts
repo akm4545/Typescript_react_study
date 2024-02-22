7832af4d3dd6c7a64347d9cba76d70b8ebdb720f
akm4545 <akm4545@naver.com>
2024-02-22 23:26:52 +0900

    chapter4

--- chapter4/discriminated_unions.ts
+++ chapter4/discriminated_unions.ts
@@ -124,3 +124,34 @@
         }
     }
 }
+
+{
+    // 배민 상품권 서비
+    // 상품권 가격에 따라 상품권 이름은 반환하는 함수
+    type ProductPrice = "10000" | "20000";
+
+    const getProductName = (productPrice: ProductPrice): string => {
+        if(productPrice === "10000") return "배민상품권 1만 원";
+        if(productPrice === "20000") return "배민상품권 2만 원";
+        else{
+            return "배민상품권";
+        }
+    };
+
+    // 새로운 상품권이 생겼다고 가정하면
+    type ProductPrice1 = "10000" | "20000" | "5000";
+
+    const getProductName1 = (productPrice: ProductPrice1): string => {
+        if(productPrice === "10000") return "배민상품권 1만 원";
+        if(productPrice === "20000") return "배민상품권 2만 원";
+        // if(productPrice === "5000") return "배민상품권 5천 원";
+        else{
+            exhaustiveCheck(productPrice); //Error: Argument of type 'string' is not assingable to parameter of type 'never'
+            return "배민상품권";
+        }
+    };
+
+    const exhaustiveCheck = (param: never) => {
+        throw new Error("type error!");
+    };
+}
