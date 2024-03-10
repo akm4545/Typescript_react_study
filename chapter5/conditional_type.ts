eac6e2dfcf1f8b9f2c1ee89a29a17a6236856d1b
akm4545 <akm4545@naver.com>
2024-03-10 19:08:05 +0900

    chapter5

--- chapter5/conditional_type.ts
+++ chapter5/conditional_type.ts
@@ -146,5 +146,125 @@
 } 
 
 {
-    
+    // infer = 추론하다 라는 의미를 지니고 있다
+    // 타입을 추론하는 역할을 한다 
+    // 삼항 연산자를 사용한 조건문의 형태를 가지는데 extends로 조건을 서술하고 infer로 타입을 추론하는 방식을 취한다
+
+    // UnpackPromise 타입은 제네릭으로 T를 받아 (type UnpackPromise<T>)
+    // T가 Promise로 래핑된 경우라면 (T extends Promise<infer K>[])
+    // K를 반환하고 그렇지 않은 경우에는 any 반환 (? K : any)
+
+    // Promise<infer K>는 Promise의 반환 값을 추론해 해당 값의 타입을 K로 한다는 의미이다
+    type UnpackPromise<T> = T extends Promise<infer K>[] ? K : any;
+
+    const promises = [Promise.resolve("Mark"), Promise.resolve(38)];
+    type Expected = UnpackPromise<typeof promises>; //string | number
+}
+
+{
+    // 배민 라이더를 관리하는 라이더 어드민 서비스에서 사용하는 타입으로 infer를 사용
+
+    // RouteBase, RouteItem = 라이더 어드민에서 라우팅을 위해 사용하는 타입
+    // routes같이 배열 형태로 사용
+
+    // 권한 API로 반환된 사용자 권한과 name을 비교하여 인가되지 않은 사용자의 접근 방지
+    // RouteItem의 name은 pages가 있을 때는 단순히 이름의 역할만 하며 그렇지 않을 때는 사용자 권한과 비교
+    interface RouteBase {
+        name: string;
+        path: string;
+        component: ComponentType;
+    }
+
+    export interface RouteItem{
+        name: string;
+        path: string;
+        component?: ComponentType;
+        pages?: RouteBase[];
+    }
+
+    export const routes: RouteItem[] = [
+        {
+            name: "기기 내역 관리",
+            path: "/device-history",
+            component: DeviceHistoryPage,
+        },
+        {
+            name: "헬멧 인증 관리",
+            path: "/helmet-certification",
+            component: HelmetCertificationPage,
+        },
+        //...
+    ];
+
+    // MainMenu, SubMenu는 메뉴 리스트에서 사용하는 타입으로 권한 API를 통해 반환된 사용자 권한과 
+    // name을 비교하여 사용자가 접근할 수 있는 메뉴만 렌더링
+    export interface SubMenu{
+        name: string;
+        path: string;
+    }
+
+    // MainMenu의 name은 subMenus를 가지고 있을 때 단순히 이름의 역할만 하며 그렇지 않을 때는 권한으로 간주
+    export interface MainMenu{
+        name: string;
+        path?: string;
+        subMenus?: SubMenu[];
+    }
+
+    // item들의 권한 체크를 위해서는 동일한 문자열을 입력해야 하는 제약이 존재
+    // name의 타입이 string 이므로 서로 다른 값이 입력되어도 컴파일 타입에서 에러가 발생하지 않는다
+    // 또한 런타임에서도 인가되지 않음을 안내하는 페이지를 보여주거나 
+    // 메뉴 리스트를 렌더링하지 않는 정도에 그치기 떄문에 존재하지 않는 권한에 대한 문제로 잘 못 인지할 수 있다
+    export type MenuItem = MainMenu | SubMenu;
+    export const menuList: MenuItem[] = [
+        {
+            name: "계정 관리",
+            subMenus: [
+                {
+                    name: "기기 내역 관리",
+                    path: "/device-history",
+                },
+                {
+                    name: "헬멧 인증 관리",
+                    path: "/helmet-certification"
+                },
+            ],
+        },
+        {
+            name: "운행 관리",
+            path: "/operation",
+        },
+        // ...
+    ];
+
+    // 이를 개선하기 위해 PermissionNames처럼 별도 타입을 선언하여 name을 관리하는 방법도 있지만
+    // 권한 검사가 필요없는 subMenus나 pages가 존재하는 name은 따로 처리해야 한다 (즉 name이 권한의 역할이 아닌 이름의 역할이 부여되었을 떄) 
+    type PermissionNames = "기기 정보 관리" | "안전모 인증 관리" | "운행 여부 조회";
+
+    export interface MainMenu {
+        //...
+        subMenus?: ReadonlyArray<SubMenu>;
+    }
+
+    export const menuList = [
+        // ...
+    ] as const;
+
+    interface RouteBase {
+        name: PermissionNames;
+        path: string;
+        component: ComponentType;
+    }
+
+    export type RouteItem = 
+        | {
+            name: string;
+            path: string;
+            component?: ComponentType;
+            pages: RouteBase[];
+        }
+        | {
+            name: PermissionNames;
+            path: string;
+            component?: COmponentType;
+        }
 }
