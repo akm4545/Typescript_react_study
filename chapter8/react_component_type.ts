d8fd753c49d5aa514b7b601ec47797d02ba3fbd8
akm4545 <akm4545@naver.com>
2024-03-26 18:41:54 +0900

    chapter8

--- chapter8/react_component_type.ts
+++ chapter8/react_component_type.ts
@@ -214,5 +214,131 @@
     // ReactElement 타입은 JSX의 createElement 메서드 호출로 생성된 리액트 엘리먼트를 나타내는 타입이라고 볼 수 있다
 }
 
+{
+    // ReactChild 타입
+    // ReactChild 타입은 ReactElement | string | number 로 정의되어 ReactElement 보다는 좀 더 넓은 범위를 갖고 있다
+    type ReactText = string | number;
+    type ReactChild = ReactElement | ReactText;
+
+    type ReactFragment = {} | Iterable<ReactNode>; //ReactNode의 배열 형태
+
+    // ReactNode는 render 함수가 반환할 수 있는 모든 형태를 담고 있다
+    type ReactNode = 
+        | ReactChild 
+        | ReactFragment
+        | ReactPortal
+        | boolean
+        | null
+        | undefined;
+}
+
+{
+    // JSX.Element는 ReactElement의 특정 타입으로 props와 타입 필드를 any로 가지는 타입
+    declare global {
+        namespace JSX {
+            interface Element extends React.ReactElement<any, any> {
+                //...
+            }
+            //...
+        }
+    }
+}
+
+{
+    // children을 포함하는 props 타입 
+    interface MyComponentProps {
+        children?: React.ReactNode;
+        //...
+    }
+
+    // JSX 형태의 문법을 때로는 string, number, null, undefined 같이 어떤 타입이든 children prop으로 지정할 수 있게 하고 싶다면
+    // ReactNode 타입으로 children을 선언하면 된다
+    // 이런 식으로 ReactNode는 prop으로 리액트 컴포넌트가 다양한 형택를 가질 수 있게 하고 싶을 때 유용하다
+
+    // 리액트 내장 타입인 PropsWithChildren 타입도 ReactNode 타입으로 children을 선언하고 있다
+    type PropsWithChildren<P = unknown> = P & {
+        children?: ReactNode | undefined;
+    };
+
+    interface MyProps {
+        //...
+    }
+
+    type MyComponentProps = PropsWithChildren<MyProps>;
+}
+
+{
+    // JSX.Element는 props와 타입 필드가 any타입인 리액트 엘리먼트
+    // 리액트 엘리먼트를 prop으로 전달받아 render props 패턴으로 컴포넌트를 구현할 때 유용하게 활용할 수 있다
+
+    interface Props{
+        icon: JSX.Element;
+    }
+
+    const Item = ({icon}: Props) => {
+        //prop으로 받은 컴포넌트의 props에 접근할 수 있다
+        const iconSize = icon.props.size;
+
+        return (<li>{icon}</li>);
+    };
+
+    // icon prop에는 JSX.Element 타입을 가진 요소만 할당할 수 있다
+    const App = () => {
+        return <Item icon={<Icon size={14 />}}/>
+    };
+}
+
+{
+    // JSX.Element 예시를 확장하여 추론 관점에서 더 유용하게 사용 = ReactElement 사용
+    // 원하는 컴포넌트의 props를 ReactElement의 제네릭으로 지정할 수 있다
+    // 만약 JSX.Element가 ReactElement의 props 타입으로 any가 지정되었다면 ReactElement 타입을 활용하여 제네릭에 직접 해당 컴포넌트의 props 타입을 명시
+
+    interface IconProps {
+        size: number;
+    }
+
+    interface props {
+        // ReactElement의 props 타입으로 IconProps 타입 지정
+        icon: React.ReactElement<IconProps>;
+    }
+
+    // 이처럼 코드를 작성하면 icon.props에 접근할 때 어떤 props가 있는지가 추론되어 IDE에 표시된다
+    const item = ({icon}: Props) => {
+        // icon prop으로 받은 컴포넌트의 props에 접근하면 props의 목록이 추론된다
+        const iconSize = icon.props.size;
+
+        return <li>{icon}</li>;
+    };
+}
+
+{
+    // HTML button 태그를 확장한 Button 컴포넌트
+    // 새롭게 만든 컴포넌트도 기존 button 처럼 onClick 이벤트 핸들러를 지원해야만 일관성과 편의성을 모두 챙길 수 있다
+    const SquareButton = () => <button>정사각형 버튼</button>;
+}
+
+{
+    // HTML 태그의 속성 타입을 활용하는 대표적인 2가지 방법 
+    // DetailedHTMLProps, ComponentWithoutRef
+
+    // React.DetailedHTMLProps를 활용하는 경우 아래와 같이 쉽게 HTML 태그 속성과 호환되는 타입 선언 가능
+    type NativeButtonProps = React.DetailedHTMLProps<
+        React.ButtonHTMLAttributes<HTMLButtonElement>,
+        HTMLButtonElement
+    >;
+
+    // ButtonProps의 onClick 타입은 실제 HTML button 태그의 onClick이벤트 핸들러 타입과 동일하게 할당된다
+    type ButtonProps = {
+        onClick?: NativeButtonProps["onClick"];
+    };
+
+    // React.ComponentPropsWithoutRef 타입
+    type NativeButtonType = React.ComponentPropsWithoutRef<"button">;
+    // 마찬가지로 onClick 이벤트 핸들러에 대한 타입 할당
+    type ButtonProps = {
+        onClick?: NativeButtonType["onClick"];
+    };
+}
+
 
 
